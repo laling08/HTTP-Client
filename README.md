263dd80dad8dc653c664083247959a36546b8117
ishi 3 <ishiliagilcedeslabrador@gmail.com>
2025-11-27 21:49:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# HTTP-client
+
+This project, named **HTTP-client**, is a web-based client application designed to interact with a previously developed RESTful data exchange web service. It provides a user-friendly interface for consuming and displaying data from the service.
+
+## Key Features
+
+- **Web-based User Interfaces**  
+  Built with HTML5 and styled using Bootstrap, offering responsive and intuitive webpages.
+
+- **RESTful Service Interaction**  
+  Utilizes the Fetch API for seamless communication with the backend web service.
+
+- **Data Processing and Rendering**  
+  Fetched data is processed and dynamically rendered into HTML for clear presentation.
+
+- **Interactive Forms**  
+  Includes various HTML form elements (e.g., input fields, dropdown menus) to allow users to input data such as resource IDs and filtering criteria for targeted requests.
---
+++ slim-template-main/config/defaults.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+// Application's default settings
+
+// Error reporting
+// Default settings: disable all error reporting for production.
+error_reporting(0);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+// Timezone
+date_default_timezone_set('America/Toronto');
+
+
+function myCustomErrorHandler(int $error_no, string $error_message, string $file, int $line)
+{
+    echo sprintf('Oh snap! Something went wrong... Error: #%d occurred in %s at line %d: Message: %s ', $error_no, $file, $line, $error_message);
+}
+
+set_error_handler('myCustomErrorHandler');
+
+$settings = [];
+
+// Error handler
+$settings['error'] = [
+    // Should be set to false for the production environment
+    'display_error_details' => false,
+];
+
+// Logger settings
+$settings['logger'] = [
+    // Log file location
+    'path' => realpath(__DIR__ . '/../var/logs'),
+    // Default log level
+    'level' => Psr\Log\LogLevel::DEBUG,
+];
+
+// Database settings
+$settings['db'] = [
+    'host' => 'localhost',
+    'encoding' => 'utf8mb4',
+    'options' => [
+        // Turn off persistent connections
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => false,
+        // Leave column names as returned by the database driver.
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        // Set default fetch mode to array
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Set character set
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+    ]
+];
+
+return $settings;
