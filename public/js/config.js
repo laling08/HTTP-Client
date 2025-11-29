e2e1f8d876f75699e9b1d468b22128043a159410
ishi 3 <ishiliagilcedeslabrador@gmail.com>
2025-11-28 21:41:10 -0500

    Changed the broken link to the right one

--- public/js/config.js
+++ public/js/config.js
@@ -11,7 +11,7 @@
 export const CONFIG = {
   WELLBEING_API_BASE_URL: "http://localhost/wellbeing-api",
   HW1_API_BASE_URL: "http://localhost/hw1",
-  SPORTS_DB_API_URL: "https://www.thesportsdb.com/api/v1/json/3",
+  SPORTS_DB_API_URL: "https://www.thesportsdb.com/api/v1/json/3/search_all_leagues.php",
   PAGINATION: {
     DEFAULT_PAGE_SIZE:10,
     MAX_PAGE_SIZE: 50,
