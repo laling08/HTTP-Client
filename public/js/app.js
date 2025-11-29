c1a060b136a1a580f2a1258420f8a80cccf855f7
ishi 3 <ishiliagilcedeslabrador@gmail.com>
2025-11-28 22:08:38 -0500

    main application

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview Main application module for Assignment 2 HTTP Client
+ * @description Handles all UI interaction and API calls for Health Services,
+ *              Vendor Switches, and TheSportsDB endpoints
+ * @module application
+ * @author Ishilia Gilcedes V. Labrador (2242125)
+ */
